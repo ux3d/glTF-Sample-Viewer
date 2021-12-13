d91789039b0bca2430f8faa3991b847414201b28
Mathias Kanzler <kanzler@ux3d.io>
2021-12-13 16:49:29 +0100

    Add iridescence debug output

--- source/GltfState/gltf_state.js
+++ source/GltfState/gltf_state.js
@@ -175,6 +175,13 @@ GltfState.DebugOutput = {
         TRANSMISSION_FACTOR: "Transmission Factor",
         VOLUME_THICKNESS: "Volume Thickness",
     },
+
+    /** output specular lighting */
+    iridescence: {
+        IRIDESCENCE: "Iridescence",
+        IRIDESCENCE_FACTOR: "Iridescence Factor",
+        IRIDESCENCE_THICKNESS: "Iridescence Thickness",
+    },
 };
 
 export { GltfState };
